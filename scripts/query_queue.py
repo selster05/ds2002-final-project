87969e861d567d43bb631cefdbddadcb9241cf85
claudia <claudiacastagna4@gmail.com>
2026-04-22 09:29:47 -0400

    Updates query_queue.py, removing slurm job and simplfying to just outputting text file of unprocessed urls

--- scripts/query_queue.py
+++ scripts/query_queue.py
@@ -4,10 +4,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config.db_config import DB_CONFIG
 import mysql.connector
 import subprocess
-#Run this file on HPC to find every unprocessed url and submit job array.
-def query_unprocessed(output_dir):
-    os.makedirs(output_dir, exist_ok=True)
-    output_file = os.path.join(output_dir, "unprocessed_urls.txt")
+#Run this file on HPC to find every unprocessed url.
+def query_unprocessed():
+    output_file = "/standard/siller/ds2002/datadivers/unprocessed_urls.txt"
 
     conn = mysql.connector.connect(**DB_CONFIG)
     cursor = conn.cursor()
@@ -28,16 +27,6 @@ def query_unprocessed(output_dir):
 
     print(f" {len(urls)} URL,written to {output_file}")
 
-    n = len(urls)
-    subprocess.run([
-        "sbatch",
-        f"--array=1-{n}",
-        "slurm/job_array.slurm",
-        output_dir
-    ])
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("arguments: python query_queue.py output directory")
-        sys.exit(1)
-    query_unprocessed(sys.argv[1])
+    
+    query_unprocessed()
